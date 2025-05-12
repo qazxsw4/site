9523bf6cf70b7be5e641dfbcda2c488c33a4c24d
qazxsw4 <36344081+qazxsw4@users.noreply.github.com>
2025-05-12 19:03:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,6 @@
     <title></title>
 </head>
 <body>
-<?php include"header.php" ?>
-<?php include"gallery_cafe.html" ?>
-<?php include"galley_product.html" ?>
-<?php include"order.html" ?>
+<h1>Привет</h1>
 </body>
 </html>
